e789a018b8bbe209d5972af512b10fb3cd5897cf
Yorrick Bakker <y.bakker@me.com>
2021-05-14 12:56:24 +0200

    Add client side authentication

--- WhereBNB.Client/Program.cs
+++ WhereBNB.Client/Program.cs
@@ -3,10 +3,12 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Http;
 
 namespace WhereBNB.Client
 {
@@ -17,7 +19,20 @@ namespace WhereBNB.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("WhereBNB.API",
+                    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+                .CreateClient("WhereBNB.API"));
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add(
+                    "https://wherebnb.onmicrosoft.com/c1630e05-8a57-4c1d-9d7d-c1dd13514f8b/management");
+                options.ProviderOptions.LoginMode = "redirect";
+            });
 
             await builder.Build().RunAsync();
         }
